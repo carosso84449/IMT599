c77200c795e169b8d9f379eb39eedb543598a306
wenyiaw <49170609+wenyiaw@users.noreply.github.com>
2022-08-24 04:21:30 -0700

    Add files via upload

---
+++ ElectionGuard_demo/obj/Any CPU/Release/Package/PackageTmp/app/views.py
@@ -0,0 +1,62 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.views.generic import TemplateView, ListView
+from .models import Spoiled, Submitted
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def faq(request):
+    """Renders the FAQ page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/faq.html',
+    )
+
+class SearchPageView(TemplateView):
+    template_name = 'app/search.html'
+
+class SearchResultsView(ListView):
+    model = Spoiled
+    template_name = 'app/search_results.html'
